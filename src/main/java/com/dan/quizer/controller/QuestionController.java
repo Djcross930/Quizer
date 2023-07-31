6a96834465ab2d7b0a9e2b8fc0ddbf515550a046
Dan Cross <Dancross96@gmail.com>
2023-07-31 11:23:37 -0500

    added question POST method

--- src/main/java/com/dan/quizer/controller/QuestionController.java
+++ src/main/java/com/dan/quizer/controller/QuestionController.java
@@ -2,10 +2,8 @@ package com.dan.quizer.controller;
 import com.dan.quizer.Question;
 import com.dan.quizer.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -17,8 +15,12 @@ public class QuestionController {
     public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
     }
-@GetMapping("category/{category}")
+    @GetMapping("category/{category}")
     public List<Question> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
+    }
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
     }
 }
