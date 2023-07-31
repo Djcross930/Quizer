29f0419215572d946fd386f6c3ae97906e2bdba5
Dan Cross <Dancross96@gmail.com>
2023-07-31 10:47:55 -0500

    added findByCategory method

--- src/main/java/com/dan/quizer/controller/QuestionController.java
+++ src/main/java/com/dan/quizer/controller/QuestionController.java
@@ -3,6 +3,7 @@ import com.dan.quizer.Question;
 import com.dan.quizer.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -15,5 +16,9 @@ public class QuestionController {
     @GetMapping("allQuestions")
     public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
+    }
+@GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
     }
 }
