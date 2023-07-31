5f8713aa2a107d7c0bf58c1c48ec21df85de7a04
Dan Cross <Dancross96@gmail.com>
2023-07-31 10:37:46 -0500

    added another property to model, and made the get all questions method

--- src/main/java/com/dan/quizer/controller/QuestionController.java
+++ src/main/java/com/dan/quizer/controller/QuestionController.java
@@ -1,16 +1,19 @@
 package com.dan.quizer.controller;
+import com.dan.quizer.Question;
 import com.dan.quizer.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
 @RestController
 @RequestMapping("question")
 public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
-        return questionService.getAllQuestions;
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
     }
 }
--- src/main/java/com/dan/quizer/service/QuestionService.java
+++ src/main/java/com/dan/quizer/service/QuestionService.java
@@ -1,7 +1,17 @@
 package com.dan.quizer.service;
 
+import com.dan.quizer.Question;
+import com.dan.quizer.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
 }
