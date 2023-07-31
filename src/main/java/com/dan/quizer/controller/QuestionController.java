7bebc0963edff54a2b483e962f409f7b66432cec
Dan Cross <Dancross96@gmail.com>
2023-07-31 14:01:17 -0500

    added error handling for get all questions method

--- src/main/java/com/dan/quizer/controller/QuestionController.java
+++ src/main/java/com/dan/quizer/controller/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,7 +16,12 @@ public class QuestionController {
     QuestionService questionService;
     @GetMapping("allQuestions")
     public ResponseEntity<List<Question>> getAllQuestions(){
-        return questionService.getAllQuestions();
+        try {
+            return questionService.getAllQuestions();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
     @GetMapping("category/{category}")
     public List<Question> getQuestionsByCategory(@PathVariable String category){
